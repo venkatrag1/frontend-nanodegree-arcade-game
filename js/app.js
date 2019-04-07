d347087a44cc5df3c112b021b5ed3409d8f123c2
venkatrag1 <venkatraghavan.r@gmail.com>
2019-04-07 23:27:41 +0000

    render basic canvas

--- js/app.js
+++ js/app.js
@@ -24,12 +24,18 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+let Player = function() {
+    this.sprite = 'images/char-boy.png';
+};
 
+Player.prototype.update = function() {
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+let allEnemies = [];
+let player = new Player();
 
 
 // This listens for key presses and sends the keys to your
